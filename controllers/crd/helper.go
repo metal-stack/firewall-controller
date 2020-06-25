d37a38e1ea59faab113c5e5a721e8b33ea9930aa
Markus Fensterer <markus.fensterer@gmail.com>
2020-06-25 09:29:47 +0200

    fixes golangci-lint issues

--- controllers/crd/helper.go
+++ controllers/crd/helper.go
@@ -17,44 +17,6 @@ func init() {
 	_ = apiextensionsv1beta1.AddToScheme(crdScheme)
 }
 
-// mergePaths merges two string slices containing paths.
-// This function makes no guarantees about order of the merged slice.
-func mergePaths(s1, s2 []string) []string {
-	m := make(map[string]struct{})
-	for _, s := range s1 {
-		m[s] = struct{}{}
-	}
-	for _, s := range s2 {
-		m[s] = struct{}{}
-	}
-	merged := make([]string, len(m))
-	i := 0
-	for key := range m {
-		merged[i] = key
-		i++
-	}
-	return merged
-}
-
-// mergeCRDs merges two CRD slices using their names.
-// This function makes no guarantees about order of the merged slice.
-func mergeCRDs(s1, s2 []runtime.Object) []runtime.Object {
-	m := make(map[string]*unstructured.Unstructured)
-	for _, obj := range runtimeCRDListToUnstructured(s1) {
-		m[obj.GetName()] = obj
-	}
-	for _, obj := range runtimeCRDListToUnstructured(s2) {
-		m[obj.GetName()] = obj
-	}
-	merged := make([]runtime.Object, len(m))
-	i := 0
-	for _, obj := range m {
-		merged[i] = obj
-		i++
-	}
-	return merged
-}
-
 func runtimeCRDListToUnstructured(l []runtime.Object) []*unstructured.Unstructured {
 	res := []*unstructured.Unstructured{}
 	for _, obj := range l {
