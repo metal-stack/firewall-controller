0aa490e57a61f7454dc52b07b34cd5fdeece08cf
Markus Fensterer <markus.fensterer@gmail.com>
2021-04-01 13:19:03 +0200

    provide doc.go in statik pkg

---
+++ pkg/network/statik/doc.go
@@ -0,0 +1,5 @@
+// Package statik provides vfs access to static content
+// content here is generated with the statik command from the make process.
+// to ensure manifest generation works even without the statik.go, this package must be present.
+// because the content of statik.go is generated from the manifests
+package statik
