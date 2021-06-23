pratheep73/Kubctl

cnwp controller

pkg/nftables/firewall.go
@@ -10,6 +10,7 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/hashicorp/go-multierror"
 	firewallv1 "github.com/metal-stack/firewall-controller/api/v1"
+	"github.com/metal-stack/firewall-controller/pkg/dns"
 
 	mn "github.com/metal-stack/metal-lib/pkg/net"
 	"github.com/vishvananda/netlink"
@@ -38,6 +39,7 @@ type Firewall struct {
 
 	primaryPrivateNet *firewallv1.FirewallNetwork
 	networkMap        networkMap
+	cache             *dns.DNSCache
 
 	dryRun bool
 }
@@ -54,11 +56,17 @@ type forwardingRules struct {
 // NewDefaultFirewall creates a new default nftables firewall.
 func NewDefaultFirewall() *Firewall {
 	defaultSpec := firewallv1.FirewallSpec{}
-	return NewFirewall(&firewallv1.ClusterwideNetworkPolicyList{}, &v1.ServiceList{}, defaultSpec, nil)
+	return NewFirewall(&firewallv1.ClusterwideNetworkPolicyList{}, &v1.ServiceList{}, defaultSpec, nil, nil)
 }
 
 // NewFirewall creates a new nftables firewall object based on k8s entities
-func NewFirewall(nps *firewallv1.ClusterwideNetworkPolicyList, svcs *corev1.ServiceList, spec firewallv1.FirewallSpec, log logr.Logger) *Firewall {
+func NewFirewall(
+	nps *firewallv1.ClusterwideNetworkPolicyList,
+	svcs *corev1.ServiceList,
+	spec firewallv1.FirewallSpec,
+	cache *dns.DNSCache,
+	log logr.Logger,
+) *Firewall {
 	networkMap := networkMap{}
 	var primaryPrivateNet *firewallv1.FirewallNetwork
 	for i, n := range spec.FirewallNetworks {
@@ -78,6 +86,7 @@ func NewFirewall(nps *firewallv1.ClusterwideNetworkPolicyList, svcs *corev1.Serv
 		primaryPrivateNet:          primaryPrivateNet,
 		networkMap:                 networkMap,
 		dryRun:                     spec.DryRun,
+		cache:                      cache,
 		log:                        log,
 	}
 }

metal-stack/firewall-controller

pkg/nftables/firewall.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/hashicorp/go-multierror"
 	firewallv1 "github.com/metal-stack/firewall-controller/api/v1"
+	"github.com/metal-stack/firewall-controller/pkg/dns"
 
 	mn "github.com/metal-stack/metal-lib/pkg/net"
 	"github.com/vishvananda/netlink"
@@ -38,6 +39,7 @@ type Firewall struct {
 
 	primaryPrivateNet *firewallv1.FirewallNetwork
 	networkMap        networkMap
+	cache             *dns.DNSCache
 
 	dryRun bool
 }
@@ -54,11 +56,17 @@ type forwardingRules struct {
 // NewDefaultFirewall creates a new default nftables firewall.
 func NewDefaultFirewall() *Firewall {
 	defaultSpec := firewallv1.FirewallSpec{}
-	return NewFirewall(&firewallv1.ClusterwideNetworkPolicyList{}, &corev1.ServiceList{}, defaultSpec, nil)
+	return NewFirewall(&firewallv1.ClusterwideNetworkPolicyList{}, &corev1.ServiceList{}, defaultSpec, nil, nil)
 }
 
 // NewFirewall creates a new nftables firewall object based on k8s entities
-func NewFirewall(nps *firewallv1.ClusterwideNetworkPolicyList, svcs *corev1.ServiceList, spec firewallv1.FirewallSpec, log logr.Logger) *Firewall {
+func NewFirewall(
+	nps *firewallv1.ClusterwideNetworkPolicyList,
+	svcs *corev1.ServiceList,
+	spec firewallv1.FirewallSpec,
+	cache *dns.DNSCache,
+	log logr.Logger,
+) *Firewall {
 	networkMap := networkMap{}
 	var primaryPrivateNet *firewallv1.FirewallNetwork
 	for i, n := range spec.FirewallNetworks {
@@ -78,6 +86,7 @@ func NewFirewall(nps *firewallv1.ClusterwideNetworkPolicyList, svcs *corev1.Serv
 		primaryPrivateNet:          primaryPrivateNet,
 		networkMap:                 networkMap,
 		dryRun:                     spec.DryRun,
+		cache:                      cache,
 		log:                        log,
 	}
 }