1e8a42df08837f140db127f5120de5cad92e6618
Markus Wennrich <markus.wennrich@f-i-ts.de>
2023-04-21 13:40:50 +0200

    use secrets generated by secretsmanager

--- controllers/droptailer_controller.go
+++ controllers/droptailer_controller.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
+	"strconv"
 	"time"
 
 	"sigs.k8s.io/controller-runtime/pkg/builder"
@@ -18,17 +20,16 @@ import (
 	"github.com/txn2/txeh"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
-	secretName              = "droptailer-client"     //nolint:gosec
-	secretKeyCertificate    = "droptailer-client.crt" //nolint:gosec
-	secretKeyCertificateKey = "droptailer-client.key" //nolint:gosec
-	secretKeyCaCertificate  = "ca.crt"                //nolint:gosec
+	secretName              = "droptailer-client" //nolint:gosec
+	secretKeyCertificate    = "tls.crt"           //nolint:gosec
+	secretKeyCertificateKey = "tls.key"           //nolint:gosec
+	secretKeyCaCertificate  = "ca.crt"            //nolint:gosec
 	defaultCertificateBase  = "/etc/droptailer-client"
 )
 
@@ -139,22 +140,21 @@ func (r *DroptailerReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		return ctrl.Result{}, nil
 	}
 
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      secretName,
-			Namespace: firewallv1.ClusterwideNetworkPolicyNamespace,
-		},
+	secret, err := getLatestSecret(ctx, r.ShootClient, firewallv1.ClusterwideNetworkPolicyNamespace, secretName)
+	if err != nil {
+		return ctrl.Result{}, err
 	}
-	if err := r.ShootClient.Get(ctx, client.ObjectKeyFromObject(secret), secret); err != nil {
-		if apierrors.IsNotFound(err) {
-			r.Log.Info("droptailer-secret not found")
-			return ctrl.Result{}, nil
-		}
-
+	err = r.writeSecret(secret)
+	if err != nil {
 		return ctrl.Result{}, err
 	}
 
-	err := r.writeSecret(secret)
+	// ugly migration code to for secretsmanager-secrets, remove when there is no firewall-image older than 2023-05 in use
+	err = r.removeAndLinkCert(defaultCertificateBase, "droptailer-client.crt", secretKeyCertificate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	err = r.removeAndLinkCert(defaultCertificateBase, "droptailer-client.key", secretKeyCertificateKey)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
@@ -172,9 +172,34 @@ func (r *DroptailerReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		r.oldPodIP = podIP
 	}
 
+	err = exec.Command("systemctl", "restart", "droptailer.service").Run()
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
 	r.Log.Info("droptailer successfully reconciled")
 
 	return ctrl.Result{}, nil
+}
+
+func (r *DroptailerReconciler) removeAndLinkCert(base, old, new string) error {
+	newFilename := path.Join(base, new)
+	_, err := os.Stat(newFilename)
+	if os.IsNotExist(err) {
+		// new file does not exist, nothing to do
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	oldFilename := path.Join(base, old)
+	if err := os.Remove(oldFilename); err != nil {
+		r.Log.Info("could not remove old file: %s", oldFilename)
+	}
+	if err := os.Symlink(newFilename, oldFilename); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *DroptailerReconciler) writeSecret(secret *corev1.Secret) error {
@@ -195,4 +220,52 @@ func (r *DroptailerReconciler) writeSecret(secret *corev1.Secret) error {
 		}
 	}
 	return nil
+}
+
+func getLatestSecret(ctx context.Context, c client.Client, namespace string, name string) (*corev1.Secret, error) {
+	secretList := &corev1.SecretList{}
+	if err := c.List(ctx, secretList, client.InNamespace(namespace), client.MatchingLabels{
+		"name":       name,
+		"managed-by": "secrets-manager",
+	}); err != nil {
+		return nil, err
+	}
+
+	return getLatestIssuedSecret(secretList.Items)
+}
+
+// getLatestIssuedSecret returns the secret with the "issued-at-time" label that represents the latest point in time
+func getLatestIssuedSecret(secrets []corev1.Secret) (*corev1.Secret, error) {
+	if len(secrets) == 0 {
+		return nil, fmt.Errorf("no secret found")
+	}
+
+	var newestSecret *corev1.Secret
+	var currentIssuedAtTime time.Time
+	for i := 0; i < len(secrets); i++ {
+		// if some of the secrets have no "issued-at-time" label
+		// we have a problem since this is the source of truth
+		issuedAt, ok := secrets[i].Labels["issued-at-time"]
+		if !ok {
+			// there are some old secrets from ancient gardener versions which have to be skipped... (e.g. ssh-keypair.old)
+			continue
+		}
+
+		issuedAtUnix, err := strconv.ParseInt(issuedAt, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		issuedAtTime := time.Unix(issuedAtUnix, 0).UTC()
+		if newestSecret == nil || issuedAtTime.After(currentIssuedAtTime) {
+			newestSecret = &secrets[i]
+			currentIssuedAtTime = issuedAtTime
+		}
+	}
+
+	if newestSecret == nil {
+		return nil, fmt.Errorf("no secret found")
+	}
+
+	return newestSecret, nil
 }
