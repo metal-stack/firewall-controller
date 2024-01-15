55ee37da81da5d052e0c7b57f7568bf6e8fdcc30
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2024-01-15 14:08:35 +0100

    set missing recorder

--- main.go
+++ main.go
@@ -226,6 +226,7 @@ func main() {
 		SeedClient:    seedMgr.GetClient(),
 		ShootClient:   shootMgr.GetClient(),
 		Log:           ctrl.Log.WithName("controllers").WithName("ClusterwideNetworkPolicy"),
+		Recorder:      shootMgr.GetEventRecorderFor("FirewallController"),
 		FirewallName:  firewallName,
 		SeedNamespace: seedNamespace,
 	}).SetupWithManager(shootMgr); err != nil {
