70af9ab692ec7cc4bc5b3c5f6204670ba37467a7
Valentin Knabel <dev@vknabel.com>
2024-03-22 13:40:27 +0100

    fix: initial dns for proxy

--- pkg/dns/dnsproxy.go
+++ pkg/dns/dnsproxy.go
@@ -36,8 +36,11 @@ type DNSProxy struct {
 	handler DNSHandler
 }
 
-func NewDNSProxy(port *uint, log logr.Logger) (*DNSProxy, error) {
-	cache := newDNSCache(true, false, log.WithName("DNS cache"))
+func NewDNSProxy(dns string, port *uint, log logr.Logger) (*DNSProxy, error) {
+	if dns == "" {
+		dns = defaultDNSServerAddr
+	}
+	cache := newDNSCache(dns, true, false, log.WithName("DNS cache"))
 	handler := NewDNSProxyHandler(log, cache)
 
 	host, err := getHost()
