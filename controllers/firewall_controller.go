33fa22e0b4a31e3f633b1db3cc07f990759d267f
Markus Wennrich <markus.wennrich@f-i-ts.de>
2026-01-23 12:29:29 +0100

    migrate from deprecated endpoints to endpointslices

--- controllers/firewall_controller.go
+++ controllers/firewall_controller.go
@@ -9,6 +9,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/metal-stack/metal-lib/pkg/pointer"
+	discoveryv1 "k8s.io/api/discovery/v1"
+
 	"github.com/Masterminds/semver/v3"
 	"github.com/go-logr/logr"
 	mn "github.com/metal-stack/metal-lib/pkg/net"
@@ -161,7 +164,7 @@ type firewallService struct {
 	namedPort string
 }
 
-// reconcileFirewallServices reconciles the services and endpoints exposed by the firewall
+// reconcileFirewallServices reconciles the services and endpoint slices exposed by the firewall
 func (r *FirewallReconciler) reconcileFirewallServices(ctx context.Context, f *firewallv2.Firewall) error {
 	services := []firewallService{
 		{
@@ -256,43 +259,46 @@ func (r *FirewallReconciler) reconcileFirewallService(ctx context.Context, s fir
 		return fmt.Errorf("private firewall network contains no ip")
 	}
 
-	endpoints := corev1.Endpoints{
-		ObjectMeta: meta,
-		Subsets: []corev1.EndpointSubset{
+	endpointSlice := discoveryv1.EndpointSlice{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      meta.Name,
+			Namespace: meta.Namespace,
+			Labels: map[string]string{
+				discoveryv1.LabelServiceName: meta.Name,
+			},
+		},
+		AddressType: discoveryv1.AddressTypeIPv4,
+		Endpoints: []discoveryv1.Endpoint{
 			{
-				Addresses: []corev1.EndpointAddress{
-					{
-						IP: privateNet.IPs[0],
-					},
-				},
-				Ports: []corev1.EndpointPort{
-					{
-						Name:     s.namedPort,
-						Port:     s.port,
-						Protocol: corev1.ProtocolTCP,
-					},
-				},
+				Addresses:  []string{privateNet.IPs[0]},
+				Conditions: discoveryv1.EndpointConditions{Ready: pointer.Pointer(true)},
+			},
+		},
+		Ports: []discoveryv1.EndpointPort{
+			{
+				Name:     pointer.Pointer(s.namedPort),
+				Port:     pointer.Pointer(s.port),
+				Protocol: pointer.Pointer(corev1.ProtocolTCP),
 			},
 		},
 	}
 
-	var currentEndpoints corev1.Endpoints
-	err = r.ShootClient.Get(ctx, nn, &currentEndpoints)
+	var currentEndpointSlice discoveryv1.EndpointSlice
+	err = r.ShootClient.Get(ctx, types.NamespacedName{Name: endpointSlice.Name, Namespace: endpointSlice.Namespace}, &currentEndpointSlice)
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
 
 	if apierrors.IsNotFound(err) {
-		err = r.ShootClient.Create(ctx, &endpoints)
-		if err != nil {
-			return err
-		}
-		return nil
+		return r.ShootClient.Create(ctx, &endpointSlice)
 	}
 
-	if !reflect.DeepEqual(currentEndpoints.Subsets, endpoints.Subsets) {
-		currentEndpoints.Subsets = endpoints.Subsets
-		return r.ShootClient.Update(ctx, &currentEndpoints)
+	if currentEndpointSlice.Labels == nil || !reflect.DeepEqual(currentEndpointSlice.Labels, endpointSlice.Labels) || currentEndpointSlice.AddressType != endpointSlice.AddressType || !reflect.DeepEqual(currentEndpointSlice.Ports, endpointSlice.Ports) || !reflect.DeepEqual(currentEndpointSlice.Endpoints, endpointSlice.Endpoints) {
+		currentEndpointSlice.Labels = endpointSlice.Labels
+		currentEndpointSlice.AddressType = endpointSlice.AddressType
+		currentEndpointSlice.Ports = endpointSlice.Ports
+		currentEndpointSlice.Endpoints = endpointSlice.Endpoints
+		return r.ShootClient.Update(ctx, &currentEndpointSlice)
 	}
 
 	return nil
