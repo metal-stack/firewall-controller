8531b54e446f0ae8f81e3ba8986b9ff7f8ee99b2
Markus Wennrich <markus.wennrich@f-i-ts.de>
2026-02-04 06:50:52 +0100

    fix: update channel type for reconciliation and event handling

--- controllers/clusterwidenetworkpolicy_controller.go
+++ controllers/clusterwidenetworkpolicy_controller.go
@@ -54,7 +54,7 @@ func (r *ClusterwideNetworkPolicyReconciler) SetupWithManager(mgr ctrl.Manager) 
 		r.Interval = reconciliationInterval
 	}
 
-	scheduleChan := make(chan event.TypedGenericEvent[client.Object])
+	scheduleChan := make(chan event.TypedGenericEvent[*firewallv1.ClusterwideNetworkPolicy])
 	if err := mgr.Add(r.getReconciliationTicker(scheduleChan)); err != nil {
 		return fmt.Errorf("failed to add runnable to manager: %w", err)
 	}
@@ -62,7 +62,7 @@ func (r *ClusterwideNetworkPolicyReconciler) SetupWithManager(mgr ctrl.Manager) 
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&firewallv1.ClusterwideNetworkPolicy{}, builder.WithPredicates(predicate.GenerationChangedPredicate{})).
 		Watches(&corev1.Service{}, &handler.TypedEnqueueRequestForObject[client.Object]{}).
-		WatchesRawSource(source.Channel(scheduleChan, &handler.TypedEnqueueRequestForObject[client.Object]{})).
+		WatchesRawSource(source.Channel(scheduleChan, &handler.TypedEnqueueRequestForObject[*firewallv1.ClusterwideNetworkPolicy]{})).
 		Complete(r)
 }
 
@@ -184,9 +184,9 @@ func (r *ClusterwideNetworkPolicyReconciler) manageDNSProxy(
 //  1. When it's rebooted, metal-networker will generate basic nftables config and apply it.
 //     In basic config there's now DNAT rules required for DNS Proxy.
 //  2. DNS Proxy is started by CWNP controller, and it will not be started until some CWNP resource is created/updated/deleted.
-func (r *ClusterwideNetworkPolicyReconciler) getReconciliationTicker(scheduleChan chan<- event.TypedGenericEvent[client.Object]) manager.RunnableFunc {
+func (r *ClusterwideNetworkPolicyReconciler) getReconciliationTicker(scheduleChan chan<- event.TypedGenericEvent[*firewallv1.ClusterwideNetworkPolicy]) manager.RunnableFunc {
 	return func(ctx context.Context) error {
-		e := event.TypedGenericEvent[client.Object]{Object: &firewallv1.ClusterwideNetworkPolicy{}}
+		e := event.TypedGenericEvent[*firewallv1.ClusterwideNetworkPolicy]{Object: &firewallv1.ClusterwideNetworkPolicy{}}
 		ticker := time.NewTicker(r.Interval)
 		defer ticker.Stop()
 
